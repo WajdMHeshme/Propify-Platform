c85727b190ca1a62d9369b7cdc716332b97fa3b0
unknown <wajdtitos@gmail.com>
2026-03-12 01:27:08 +0300

    integration with NGROK

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -2,10 +2,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://unilobed-palatially-selah.ngrok-free.dev/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
